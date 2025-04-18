49ecc69d1add6cf07f8e9b3dd7d42be4cf46cf53
smlles <yangjjn95@gmail.com>
2025-04-18 17:38:26 +0900

    250418

---
+++ src/AddTodo.js
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+import { Button, Grid, TextField } from '@mui/material'
+import { Margin, PaddingRounded } from '@mui/icons-material'
+
+//Button 
+//다양한 스타일(variant) :text, contained, outlined
+//색상(color) : 
+//크기(size) : small, medium , large
+// 를 지원하는 버튼 컴포넌트
+
+//Grid2
+//CSS Grid 레이아웃 기반의 그리드 시스템
+//Flexbox 기반의 기존 grid보다 행,열 제어가 직관적, 
+//rowSpacing,columnSpacing으로 간격을 조절 할 수 있다.
+//container : 그리드 컨테이너 설정
+//item : 그리드 아이템 설정 (생략해도 자동감지)
+//xs(0),sm(600),md(900),lg(1200),xl(1536) : 브레이킹포인트별 차지할 컬럼 수 (기본 12분할)
+//브레이킹 포인트 : 화면 너비 기준값
+//columns : 총 콜럼수 조정 (기본값 12)
+//rowSpacing, columnSpacing : 행 / 열 간격
+
+//TextField
+//입력(input),라벨(label),헬퍼텍스트(helperText), 
+//에러표시(error)를 한번에 처리해주는 컴포넌트
+//variant : 스타일설정
+//label : 라벨텍스트
+//helperText : 입력하단 도움텍스트
+//error : 에러표시 , true만 빨간색 강조
+//fullWidth : 가로 100% 여부
+//multiline : 여러줄 입력여부
+//rows : multiline일 떄 보이는 줄 수
+//type : text, password, email등의 입력 타입
+
+//react에서 스타일 줄 때는 js객체형식으로 준다.
+
+//props로 넘어온 내용을 받아서 사용할 준비가 됨
+const AddTodo = ({add}) =>{
+
+  const [item, setItem]= useState({title:""});
+
+  const onInputChange = (e) =>{
+    setItem({title:e.target.value})
+    console.log({title:e.target.value})
+  }
+  //버튼 눌렀을 때
+  const onButtonClick = () =>{
+    if(item.title.trim()===''){
+      alert('내용을 입력하세요.');
+      return;
+    }
+    //props로 넘어온 add함수를 여기서 호출
+    add(item); //add({title : '내용'})
+    setItem({title:""})//함수에 전달하고 나면 입력필드 비우기
+  }
+  //엔터키 = 버튼과 같은 효과
+  const enterKeyEventHandler = (e) => {
+    if(e.key == "Enter"){
+      onButtonClick();
+    }
+  }
+
+  return(
+    <Grid 
+      container 
+      style ={ {marginTop : 20} }
+      justifyContent="center"
+      alignItems="center"
+      >
+        <Grid xs={11} md={11} item style={{paddingRight : 16}}> 
+          <TextField 
+            placeholder='Add todo here' 
+            fullWidth 
+            value={item.title}
+            onChange={onInputChange} //onChange : 사용자가 키보드를 입력하거나, 
+            // 어떻게든 값이 바뀌면 실행
+            onKeyDown={enterKeyEventHandler}
+            />
+        </Grid>
+        <Grid>
+          <Button 
+            fullWidth style={{heigth:'100%'}} 
+            color="secondary" 
+            variant='outlined'
+            onClick={onButtonClick}
+            >
+              +
+          </Button>
+        </Grid>
+    </Grid>
+  )
+}
+
+export default AddTodo;
--- src/App.js
+++ src/App.js
@@ -4,22 +4,42 @@ import './App.css';
 import Todo from './Todo';
 import { Example } from './Example';
 import { Greeting,Farewell } from './Greeting';
-import {List, Paper} from '@mui/material'
+import {Container,List, Paper} from '@mui/material'
+import AddTodo from './AddTodo'
+
+//Container
+//레이아웃의 가로폭을 제한하고, 
+//중앙정렬 및 기본 패딩을 자동으로 적용해주는 컴포넌트
+//maxWidth : 최대 너비 지정 (false는 그냥 다 차지하기)
 
 function App() {
   //하나의 할 일을 객체로 관리 할 것이다.
   //id , title , done 
-  const [items, setItem] = useState([
-    {
-    id : "0", 
-    title : "Hello, world 1",
-    done:true
-  },
-  {
-    id : "1", 
-    title : "Hello, world 2",
-    done : false
-  }]) //react는 key속성값을 참조해서  리스트의 요소가 변경 될 경우, 
+  const [items, setItem] = useState([ 
+//{여기에 아이디 어쩌고 했었음  
+//}
+  ]) 
+  
+  //Todo를 직접 추가하기 위한 백엔드 콜을 대신 할 가짜 함수만들기
+  const add = (item) => {
+    //newItem 객체가 하나의 Todo
+    const newItem = {
+      ...item,//이자리에 title 들어감
+      // 스프레드연산자 ... <- 반복가능객체 (배열)나 객체의 프로퍼티를 펼친다.
+      //복사,병합,분해등의 작업을 간결하게 해주는 문법
+      id: "ID-" + items.length,
+      done:false,
+    } 
+    //상태를 변화시키는 함수를 호출하면
+    //state에 변경 사항이 화면에 적용이 된다.
+    setItem(prev => [...prev, newItem]) 
+    //prev가 없으면 useState의 초기값으로 들어감
+    //있으면 items의 이전 값으로 들어감
+    console.log("items : ",[...items,newItem])
+
+  }
+
+  //react는 key속성값을 참조해서  리스트의 요소가 변경 될 경우, 
   // 어떤 요소가 변경되었는지 빠르게 파악 할 수 있다.
   //                    //배열의 내용이 있다면 렌더링해라
   //paper 컴퍼넌트 
@@ -36,23 +56,13 @@ function App() {
   </Paper>
   return (
     <div className="App">
-    {/*  <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>*/}
-      
-      {todoItems}
-      
+      <Container maxWidth="md">
+        <AddTodo add={add}/>
+        {/*AddTodo에 add함수 전달 {add : function add(~) {~}*/}
+        <div className = "TodoList">
+          {todoItems}
+        </div>
+      </Container>
     </div>
   )
 }
